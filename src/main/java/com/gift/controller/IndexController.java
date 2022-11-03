ec2fe6d4027c1d3180d0001efb1297a9dd452e06
Uk <bmheok@naver.com>
2022-11-03 15:12:18 +0900

    index.html 페이지네이션

--- src/main/java/com/gift/controller/IndexController.java
+++ src/main/java/com/gift/controller/IndexController.java
@@ -5,6 +5,9 @@ import com.gift.dto.SellDto;
 import com.gift.entity.Sell;
 import com.gift.repository.SellRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +21,18 @@ public class IndexController {
 @Autowired
 private SellRepository sellRepository;
     @GetMapping(value = "/")
-    public String main(Model model){
-        List<Sell> sells = sellRepository.findAll();
+    public String main(@PageableDefault(size = 6) Pageable pageable, Model model){
+
+        Page<Sell> sells = sellRepository.findAll(pageable);
+
+
+        int startPage = Math.max(1, sells.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(sells.getPageable().getPageNumber()+4, sells.getTotalPages());
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("sells",sells);
+
         return "index";
     }
 
