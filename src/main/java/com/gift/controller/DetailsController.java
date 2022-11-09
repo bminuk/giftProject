34603ba30994d50d5d10074f0506bb73633c66af
Uk <bmheok@naver.com>
2022-11-09 15:41:44 +0900

    detail정보 view에서 꺼내기 가능

--- src/main/java/com/gift/controller/DetailsController.java
+++ src/main/java/com/gift/controller/DetailsController.java
@@ -1,15 +1,30 @@
 package com.gift.controller;
 
+import com.gift.entity.member.Member;
+import com.gift.entity.sell.Sell;
+import com.gift.repository.sell.SellRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/details")
 public class DetailsController {
 
-    @GetMapping(value = "/sellDetail")
-    public String sellDetail(){
+
+
+    @Autowired
+    private SellRepository sellRepository;
+
+    @GetMapping(value = "/sellDetail/{sellId}")
+    public String sellDetail(@PathVariable("sellId")Long sellId, Model model){
+        Sell sell = sellRepository.findById(sellId).get();
+        Member member = sell.getMember();
+        model.addAttribute("member",member);
+        model.addAttribute("sell",sell);
         return "/details/sellDetail";
     }
 
--- src/main/java/com/gift/repository/sell/SellRepository.java
+++ src/main/java/com/gift/repository/sell/SellRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SellRepository extends JpaRepository<Sell, Long> {
 
     Page<Sell> findAll(Pageable pageable);
+
+    Optional<Sell> findById(Long id);
 
 }
